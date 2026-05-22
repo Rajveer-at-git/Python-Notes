e979368c78fdc4071978a3a13753e42e5e493e71
Rajveer-at-git <levelingup1923@gmail.com>
2026-05-22 15:52:22 +0530

    New Module alien.py created for managing alien ships

--- Alien Invasion Game/alien_invasion.py
+++ Alien Invasion Game/alien_invasion.py
@@ -35,14 +35,7 @@ class AlienInvasion:
             
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # Get rid of the bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom < 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-            
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
@@ -81,8 +74,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+                """Update the position of bullets and get rid of old bullets."""
+                # Update bullet positions.
+                self.bullets.update()
+
+                # Get rid of the bullets that have disappeared.
+                for bullet in self.bullets.copy():
+                    if bullet.rect.bottom < 0:
+                        self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images to the screen and flip to the new screen."""
