4c92c221cd5d5ab05921b48e04fadfc8952e040d
Rajveer-at-git <levelingup1923@gmail.com>
2026-05-24 09:11:05 +0530

    image resized

--- Alien Invasion Game/alien.py
+++ Alien Invasion Game/alien.py
@@ -12,6 +12,7 @@ class Alien(Sprite):
 
         # Load the alien image and set its rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.transform.scale(self.image, (80, 80))
         self.rect = self.image.get_rect()
 
         # Start each new alien on the top left of the screen
