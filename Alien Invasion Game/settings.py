783fe82cd937f5b6fb15b767b33ef392c45263a7
Rajveer-at-git <levelingup1923@gmail.com>
2026-05-24 09:11:37 +0530

    fireable bullets limit

--- Alien Invasion Game/settings.py
+++ Alien Invasion Game/settings.py
@@ -9,13 +9,14 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship Settings
-        self.ship_speed = 1.5
+        self.ship_speed = 3
 
         # Bullet Settings
-        self.bullet_speed = 2
+        self.bullet_speed = 4
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
 
         
         
