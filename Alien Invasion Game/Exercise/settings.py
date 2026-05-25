c8bb41103beaa9034d6334ea515db043f5ed4f59
Rajveer-at-git <levelingup1923@gmail.com>
2026-05-25 20:12:16 +0530

    settings for the screen

--- Alien Invasion Game/Exercise/settings.py
+++ Alien Invasion Game/Exercise/settings.py
@@ -1,11 +1,14 @@
 class Settings:
-    """A class to store all the settings for Alien Invasion"""
+    """A class to store all settings for the Raindrop game."""
 
     def __init__(self):
-        """Initialize the game's settings """
-        # Screen Settings
+        # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 0, 100)
+        self.bg_color = (15, 25, 40)  # dark night-sky blue
 
-        
+        # Raindrop settings
+        self.drop_speed = 2.0
+        self.drop_width = 3
+        self.drop_height = 15
+        self.drop_color = (100, 180, 255)  # light blue
