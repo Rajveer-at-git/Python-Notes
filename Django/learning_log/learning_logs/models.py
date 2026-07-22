e0640c00c557b6aff8364836a6bdf6bcaa35f021
Rajveer Choudhary <levelingup1923@gmail.com>
2026-07-22 13:42:03 +0530

    topic and entry form

--- Django/learning_log/learning_logs/models.py
+++ Django/learning_log/learning_logs/models.py
@@ -20,5 +20,8 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        """Return a simple string representing the entry."""
-        return f"{self.text[:50]}..."
+        if len(self.text) >= 50:
+            """Return a simple string representing the entry."""
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
