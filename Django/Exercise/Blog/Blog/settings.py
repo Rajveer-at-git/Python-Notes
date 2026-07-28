176831af5442575fefd0f1813e25f7ceec100996
Rajveer Choudhary <levelingup1923@gmail.com>
2026-07-28 21:48:41 +0530

    added the accounts app

--- Django/Exercise/Blog/Blog/settings.py
+++ Django/Exercise/Blog/Blog/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'blogs',
-
+    'accounts',
+    
     # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# My Settings
+LOGIN_REDIRECT_URL = 'blogs:index'
+LOGOUT_REDIRECT_URL = 'blogs:index'
