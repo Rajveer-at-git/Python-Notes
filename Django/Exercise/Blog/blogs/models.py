8d2dc5fcbe11a435fe65a501b54bc00c002824f7
Rajveer Choudhary <levelingup1923@gmail.com>
2026-07-28 19:16:15 +0530

    added fields

--- Django/Exercise/Blog/blogs/models.py
+++ Django/Exercise/Blog/blogs/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
     """A Blog the user is writing/ about"""
     name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a string representation of the model"""
