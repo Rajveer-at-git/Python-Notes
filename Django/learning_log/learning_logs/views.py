05f0f1224a0e4ed8eb80f40c8975eab9106ab0f0
Rajveer Choudhary <levelingup1923@gmail.com>
2026-07-28 22:07:24 +0530

    added login_required decorater

--- Django/learning_log/learning_logs/views.py
+++ Django/learning_log/learning_logs/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+
+
+# Create your views here.
 # These views are called view functions
 def index(request): # This is a view funtion
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
+@login_required # this is a decorator, it takes the topics fn and pass it as the argument to login_required
 def topics(request):
     """Show all the topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     # These two lines are called queries for the database
     topic = Topic.objects.get(id=topic_id)  # 1
+    # Make sure the topic belongs to the current user.
+    check_topic_owner(topic, request)
+    
     entries = topic.entry_set.order_by('-date_added') # 2
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -29,15 +41,19 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
-        return redirect('learning_logs:topics')
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return redirect('learning_logs:topics')
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request)
     
     if request.method != 'POST':
         # No data submitted; create a blank form.
@@ -55,81 +71,12 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)  # . These two lines are queries
     topic = entry.topic # .
-
-    if request.method != 'POST':
-        # Initial request; pre-fill form with the current entry.
-        form = EntryForm(instance=entry)
-    else:
-    # POST data submitted; process data.
-        form = EntryForm(instance=entry, data=request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('learning_logs:topic', topic_id=topic.id)
-    
-    context = {'entry': entry, 'topic': topic, 'form': form}
-    return render(request, 'learning_logs/edit_entry.html', context)# Create your views here.
-# These views are called view functions
-def index(request): # This is a view funtion
-    """The home page for Learning Log."""
-    return render(request, 'learning_logs/index.html')
-
-def topics(request):
-    """Show all the topics"""
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
-    return render(request, 'learning_logs/topics.html', context)
-
-def topic(request, topic_id):
-    """Show a single topic and all its entries."""
-    # These two lines are called queries for the database
-    topic = Topic.objects.get(id=topic_id)  # 1
-    entries = topic.entry_set.order_by('-date_added') # 2
-    context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topic.html', context)
-
-def new_topic(request):
-    """Add a new topic."""
-    if request.method != 'POST':
-        # No data submitted; create a blank form.
-        form = TopicForm()
-    else:
-        # POST data submitted; process data.
-        form = TopicForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('learning_logs:topics')
-    # Display a blank or invalid form.
-    context = {'form': form}
-    return render(request, 'learning_logs/new_topic.html', context)
-
-def new_entry(request, topic_id):
-    """Add a new entry for a particular topic."""
-    topic = Topic.objects.get(id=topic_id)
-    
-    if request.method != 'POST':
-        # No data submitted; create a blank form.
-        form = EntryForm()
-    else:
-        # POST data submitted; process data.
-        form = EntryForm(data=request.POST)
-        if form.is_valid():
-            new_entry = form.save(commit=False)
-            new_entry.topic = topic
-            new_entry.save()
-            return redirect('learning_logs:topic', topic_id=topic_id)
-        
-    # Display a blank or invalid form.
-    context = {'topic': topic, 'form': form}
-    return render(request, 'learning_logs/new_entry.html', context)
-
-def edit_entry(request, entry_id):
-    """Edit an existing entry."""
-    entry = Entry.objects.get(id=entry_id)  # . These two lines are queries
-    topic = entry.topic # .
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
@@ -143,3 +90,8 @@ def edit_entry(request, entry_id):
     
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(topic, request):
+    """Make sure the topic belongs to the current user."""
+    if topic.owner != request.user:
+        raise Http404
